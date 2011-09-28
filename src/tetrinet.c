717dc612a884011ec1c0e838db1c9c51b9e533c6
Jordi Mallach <jordi@sindominio.net>
2011-09-28 14:28:33 +0200

    Fix build when using -Werror=format-security.

--- src/tetrinet.c
+++ src/tetrinet.c
@@ -232,7 +232,7 @@ void tetrinet_inmessage (enum inmsg_type msgtype, char *data)
             dialog = gtk_message_dialog_new (NULL, GTK_DIALOG_MODAL,
                                              GTK_MESSAGE_ERROR,
                                              GTK_BUTTONS_OK,
-                                             buf);
+                                             "%s", buf);
             gtk_dialog_run (GTK_DIALOG(dialog));
             gtk_widget_destroy (dialog);
             g_free (data_utf8);
